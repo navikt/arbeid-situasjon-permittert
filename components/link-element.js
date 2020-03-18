70a833e6beb68b196275fd980ee5c4ee141eec69
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-03-18 20:03:18 +0100

    Legger Amplitude på linker

--- components/link-element.js
+++ components/link-element.js
@@ -1,8 +1,15 @@
+import { amplitudeLogger } from '../lib/amplitude-utils'
+
+const handleClick = event => {
+  const url = event.target.href
+  amplitudeLogger('link.click', { url })
+}
+
 const LinkElement = props => {
   const { Ui, title, url } = props
   return (
     <li className='li-unstyled mb-2'>
-      <Ui.Nav.Lenke href={url}>
+      <Ui.Nav.Lenke href={url} onClick={handleClick}>
         {title}
       </Ui.Nav.Lenke>
     </li>
