c6d83b757995a99d2abdc95ab01815bf4ba1317a
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-03-20 21:09:05 +0100

    Minimerer moment ved å fjerne ubrukte locales (patch)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 const withLess = require('@zeit/next-less')
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
 
 module.exports = withLess( {
     target: 'server',
     exportTrailingSlash: true,
-    assetPrefix: process.env.BASE_PATH || ''
+    assetPrefix: process.env.BASE_PATH || '',
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      // Note: we provide webpack above so you should not `require` it
+      // Perform customizations to webpack config
+      // Important: return the modified config
+      config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+      config.plugins.push(new MomentLocalesPlugin({
+        localesToKeep: ['nb', 'pl'],
+    }))
+      return config
+    }
   })
 
 if (typeof require !== 'undefined') {
